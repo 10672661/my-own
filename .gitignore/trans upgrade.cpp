7653b6c0a6176a9f688d92f325178a2fa4a48888
10672661 <60462210+10672661@users.noreply.github.com>
2020-02-11 22:09:11 +0800

    Update trans upgrade.cpp

--- .gitignore/trans upgrade.cpp
+++ .gitignore/trans upgrade.cpp
@@ -45,7 +45,7 @@ int jud(char str[]){
 int main(){
 	char *chinese[11]={"零","一","二","三","四","五","六","七","八","九","十"};
 	char name[200],verb[200],numb[200],oper[200],name1[200],toint[200],name3[200];
-	char a[20],b[20],numb2[20],d[20],word[100],e[20],f[20],cold[100],name2[100];
+	char a[20],b[20],numb2[20],d[20],word[100],e[20],f[20],cold[100],name2[100],numb3[20];
 	char age[200],money[200];
 	int sum,sum2,sum3;//sum表示第二题年龄，sum2表示零花钱
 	scanf("%s",toint);
@@ -55,20 +55,16 @@ int main(){
 	{	
 		if(strcmp(name1,toint)==0)//判断是否是第二题
 		{
-			scanf("%s %s %s",name3,verb,numb);
-			sum2=trans(numb);
+			scanf("%s %s %s",name3,verb,numb3);
+			sum2=jud(numb3);
 		}
-		if(strcmp(name1,name)==0)//加减法运算+-+-+-+-+-+-+-+-+-+-+-++-+-+-
-		{
+		if(strcmp(name1,name)==0){//加减法运算+-+-+-+-+-+-+-+-+-+-+-++-+-+-
 			scanf("%s",oper);
-			if(strcmp(oper,"增加")==0)
-			{
+			if(strcmp(oper,"增加")==0){
 				scanf("%s",numb);
 				sum3=jud(numb);
-				sum+=sum3;
-			}
-			if(strcmp(oper,"减少")==0)
-			{
+				sum+=sum3;}
+			if(strcmp(oper,"减少")==0){
 				scanf("%s",numb);	
 				if(strlen(numb)==2)
 				{
@@ -81,63 +77,58 @@ int main(){
 		{
 			int lasttmp;
 			scanf("%s",name);
-			if(sum>=0&&sum<=10)
-			{
-				printf("%s\n",chinese[sum]);
-			}
-			else if(sum<0)
-			{
-				int under=abs(sum);
-				printf("%s%s\n","负",chinese[under]);
-			}
-			else 
-			{
-				if(sum%10==0)
-				{
-					printf("%s十\n",chinese[sum/10]);
-				}
-				else if(sum<=19)
-				{
-					printf("十%s\n",chinese[sum%10]);
-				}
-				else 
-				{
-					printf("%s十%s\n",chinese[sum/10],chinese[sum%10]);
-				}
-			}
-			scanf("%s %s %s %s %s %s %s %s %s %s",a,name,b,numb2,d,name2,word,e,f,cold);
-			lasttmp=jud(numb2);
+			lasttmp=trans(numb2);
 			int len=strlen(word);
 			int len1=strlen(cold);
-			if(sum>lasttmp)
 			{
-				for (int i=1;i<len-1; i+=2) {printf("%c%c",word[i],word[i+1]);}//去除双引号
+				if(sum>=lasttmp)
+				{	if(sum>=0&&sum<=10){
+					printf("%s\n",chinese[sum]);
+					scanf("%s %s %s %s %s %s %s %s %s %s",a,name,b,numb2,d,name2,word,e,f,cold);
+					for (int i=1;i<len-1; i+=2) {printf("%c%c",word[i],word[i+1]);}	//去除双引号
+					}
+					else {
+					if(sum%10==0){
+						printf("%s十\n",chinese[sum/10]);
+						scanf("%s %s %s %s %s %s %s %s %s %s",a,name,b,numb2,d,name2,word,e,f,cold);
+						for (int i=1;i<len-1; i+=2) {printf("%c%c",word[i],word[i+1]);}//去除双引号
+					}
+					else if(sum<=19){
+						printf("十%s\n",chinese[sum%10]);
+						scanf("%s %s %s %s %s %s %s %s %s %s",a,name,b,numb2,d,name2,word,e,f,cold);
+						for (int i=1;i<len-1; i+=2) {printf("%c%c",word[i],word[i+1]);}//去除双引号
+					}
+					else {
+						printf("%s十%s\n",chinese[sum/10],chinese[sum%10]);
+						scanf("%s %s %s %s %s %s %s %s %s %s",a,name,b,numb2,d,name2,word,e,f,cold);
+						for (int i=1;i<len-1; i+=2) {printf("%c%c",word[i],word[i+1]);}//去除双引号
+					}
+				}}
+				else if(sum<lasttmp){
+						if(sum<0)
+						{
+							int under=abs(sum);	
+							printf("%s%s\n","负",chinese[under]);
+							for(int j=1;j<len1-1;j+=2) {printf("%c%c",cold[j],cold[j+1]);}}
+						}
+				else{	
+					printf("%s\n",chinese[sum]);for(int j=1;j<len1-1;j+=2) {printf("%c%c",cold[j],cold[j+1]);}}//去除双引号
 			}
-			else if(sum<=lasttmp)
-			{
-				if(sum<0)
-				{
-					int under1=abs(sum);	
-					printf("%s%s\n","负",chinese[under1]);
-					for(int j=1;j<len1-1;j+=2) {printf("%c%c",cold[j],cold[j+1]);}
-				}
-				else
-				{	
-					for(int j=1;j<len1-1;j+=2) {printf("%c%c",cold[j],cold[j+1]);}
-				}//去除双引号
-			}		
-		}
+			}
 		else if(strcmp(name1,"如果")==0)//用于判断是否符合第二题
-		{
+		{	
 			scanf("%s %s %s %s %s %s %s %s %s",age,b,numb2,d,money,word,e,f,cold);
 			scanf("%s %s",a,age);
 			if(strcmp(a,"看看")==0)
-			{
-				if(sum>8)//第二题sum表示年龄,sum2零花钱
+			{	int z=jud(numb2);
+				if(sum>z)//第二题sum表示年龄,sum2零花钱
 				{
 					int	x=sum2+1;
 					printf("%s",chinese[x]);
 				}
+				else{
+					int t=jud(numb3);
+					printf("%s",chinese[sum2]);}
 			}
 		}
 	}
